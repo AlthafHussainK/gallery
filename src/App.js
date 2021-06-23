4de1fd4d7f2cf9f3b4465691e0628ec4d9e0507c
AlthafHussainK <althafakm@gmail.com>
2021-06-23 07:08:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Photo Gallery</h1>
+    <div>
+      <h1 className="text-3xl">Photo Gallery</h1>
     </div>
   );
 }
