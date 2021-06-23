5ce354e6fc937214704fa61b6f3fbb6900a91f5b
AlthafHussainK <althafakm@gmail.com>
2021-06-23 10:16:40 +0530

    Image search and catch error

--- src/App.js
+++ src/App.js
@@ -15,11 +15,14 @@ function App() {
       setIsLoading(false)
     })
     .catch(err => console.log(err))
-  }, [])
+  }, [term])
 
   return (
     <div className="container mx-auto">
-      <ImageSearch />
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">Oops! No Images Found.</h1>}
+
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
       {images.map(image => (
         <ImageCard key={image.id} image={image}/>
--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 
-const ImageSearch = () => {
+const ImageSearch = ({ searchText }) => {
   const [text, setText] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault() 
+    searchText(text)
+  }
 
   return (
     <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
-    <form className="w-full max-w-sm">
+    <form onSubmit={onSubmit} className="w-full max-w-sm">
       <div className="flex items-center border-b border-b-2 border-purple-500 py-2">
         <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full 
         text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
